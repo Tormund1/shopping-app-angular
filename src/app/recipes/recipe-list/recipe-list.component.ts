574ecd6b24c12a99b48f83d054e3ec469549de5a
enes.ozdemir <enes.ozdemir@epati.com.tr>
2023-09-13 17:09:51 +0300

    recipe list sayfası oluşturuldu

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Yeni Tarif', 'bu bir testtir', 'https://cdn.yemek.com/mnresize/1250/833/uploads/2023/03/sihil-mahsi-yemekcom.jpg')
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
 
 }
